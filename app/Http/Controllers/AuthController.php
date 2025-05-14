0f57cb851c45441425b7a598d2444e3b156b39f5
saezeric <128070029+saezeric@users.noreply.github.com>
2025-05-14 15:55:28 +0000

    Registro y login de usuarios + crud funcional completamente

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use App\Models\User;
+use JWTAuth;
+
 
 class AuthController extends Controller
 {
@@ -52,6 +59,62 @@ class AuthController extends Controller
         } catch (JWTException $e) {
             return response()->json(['error' => 'Could not create token', 'message' => $e->getMessage(),], 500);
         }
+    }
+
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(['message' => 'Users retrieved successfully', 'data' => $users], 200);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        return response()->json(['message' => 'User retrieved successfully', 'data' => $user], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'sometimes|required|string|max:100',
+            'role' => 'sometimes|required|string|max:100|in:admin,user',
+            'email' => [
+                'sometimes',
+                'required',
+                'string',
+                'email',
+                'max:100',
+                Rule::unique('users')->ignore($user->id),
+            ],
+            'password' => 'sometimes|required|string|min:5|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user->update($request->only(['name', 'role', 'email', 'password']));
+
+        return response()->json(['message' => 'User updated successfully', 'data' => $user], 200);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $user->delete();
+        return response()->json(['message' => 'User deleted successfully'], 200);
     }
 
     public function getUser()
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Card;
 use App\Http\Controllers\Api\CardController;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
 use App\Http\Middleware\IsUserAuth;
 use App\Http\Middleware\IsAdmin;
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -17,33 +18,27 @@ use App\Http\Middleware\IsAdmin;
 // });
 
 // PUBLIC ROUTES
-
-Route::get('register', [AuthController::class, 'register']);
-Route::get('login', [AuthController::class, 'login']);
-
-Route::get('/cards',          [CardController::class, 'index']);
-Route::get('/cards/{id}',     [CardController::class, 'show']);
-Route::post('/cards',         [CardController::class, 'store']);
-Route::put('/cards/{id}',     [CardController::class, 'update']);
-Route::patch('/cards/{id}',   [CardController::class, 'updatePartial']);
-Route::delete('/cards/{id}',  [CardController::class, 'destroy']);
+Route::post('register',  [AuthController::class, 'register']);
+Route::post('login',     [AuthController::class, 'login']);
 
 // PROTECTED ROUTES
-
 Route::middleware([IsUserAuth::class])->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('me', [AuthController::class, 'getUser']);
-    // Route::post('cards', [CardController::class, 'put']);
+    Route::post('logout',         [AuthController::class, 'logout']);
+    Route::get('me',              [AuthController::class, 'getUser']);
+    Route::get('/cards',          [CardController::class, 'index']);
+    Route::get('/cards/{id}',     [CardController::class, 'show']);
+    Route::post('cards',          [CardController::class, 'store']);
 });
 
 // ADMIN ROUTES
 Route::middleware([IsAdmin::class])->group(function () {
-    Route::get('users', [AuthController::class, 'index']);
-    Route::get('/users/{id}', [AuthController::class, 'show']);
-    Route::put('/users/{id}', [AuthController::class, 'update']);
-    Route::delete('/users/{id}', [AuthController::class, 'destroy']);
-    // cards
-    // Route::post('cards', [CardController::class, 'put']);
-    // Route::put('/cards/{id}', [CardController::class, 'update']);
-    // Route::delete('/cards/{id}', [CardController::class, 'destroy']);
+    Route::get('users',           [AuthController::class, 'index']);
+    Route::get('/users/{id}',     [AuthController::class, 'show']);
+    Route::put('/users/{id}',     [AuthController::class, 'update']);
+    Route::delete('/users/{id}',  [AuthController::class, 'destroy']);
+    // CARDS
+    Route::post('cards',          [CardController::class, 'store']);
+    Route::put('/cards/{id}',     [CardController::class, 'update']);
+    Route::patch('/cards/{id}',   [CardController::class, 'updatePartial']);
+    Route::delete('/cards/{id}',  [CardController::class, 'destroy']);
 });
