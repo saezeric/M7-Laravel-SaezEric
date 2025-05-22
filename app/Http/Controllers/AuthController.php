307e55c5728a0def043d33455f1dcf9d2218ab4c
saezeric <128070029+saezeric@users.noreply.github.com>
2025-05-22 15:19:54 +0000

    Registro, Login y Logout terminados

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:5|confirmed',
+            'role' => 'required|in:admin,user',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role' => $request->role,
+        ]);
+
+        return response()->json(['message' => 'Usuario registrado correctamente', 'user' => $user], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Credenciales incorrectas'], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login exitoso',
+            'token' => $token,
+        ], 200);
+    }
+
+    public function logout()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+            return response()->json(['message' => 'Logout exitoso'], 200);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Error al realizar logout'], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,71 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Card;
-use App\Http\Controllers\Api\CardController;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\IsUserAuth;
-use App\Http\Middleware\IsAdmin;
-use App\Http\Controllers\GameController;
-use App\Http\Controllers\CategoryController;
+use App\Http\Middleware\IsUserAdmin;
+
+// RUTAS PÚBLICAS
+Route::post('/register',    [AuthController::class, 'register']);
+Route::post('/login',       [AuthController::class, 'login']);
 
 
-
-
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-// Route::get('/cards/test-connection', function () {
-//     return Card::all();
-// });
-
-// PUBLIC ROUTES
-Route::post('register',  [AuthController::class, 'register']);
-Route::post('login',     [AuthController::class, 'login']);
-
-// PROTECTED ROUTES
 Route::middleware([IsUserAuth::class])->group(function () {
-    Route::post('logout',         [AuthController::class, 'logout']);
-    Route::get('me',              [AuthController::class, 'getUser']);
-    // CARDS
-    Route::get('/cards',          [CardController::class, 'index']);
-    Route::get('/cards/{id}',     [CardController::class, 'show']);
-    Route::post('cards',          [CardController::class, 'store']);
-    // GAMES
-    Route::get('/games', [GameController::class, 'index']);
-    Route::post('/games', [GameController::class, 'store']);
-    Route::put('/games/{game}/finish', [GameController::class, 'update']);
-    Route::delete('/games/{game}', [GameController::class, 'destroy']);
-    Route::get('/ranking', [GameController::class, 'ranking']);
-    // CATEGORIES
-    Route::get('/categories', [CategoryController::class, 'index']);
-    Route::get('/cards/category/{categoryId}', [CardController::class, 'getByCategory']); // CARDS Y CATEGORIES
-    Route::post('/categories', [CategoryController::class, 'store']);
-    Route::put('/categories/{category}', [CategoryController::class, 'update']);
-    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+    Route::post('/logout',  [AuthController::class, 'logout']);
 });
 
-// ADMIN ROUTES
-Route::middleware([IsAdmin::class])->group(function () {
-    Route::get('users',           [AuthController::class, 'index']);
-    Route::get('/users/{id}',     [AuthController::class, 'show']);
-    Route::put('/users/{id}',     [AuthController::class, 'update']);
-    Route::delete('/users/{id}',  [AuthController::class, 'destroy']);
-    // CARDS
-    Route::post('cards',          [CardController::class, 'store']);
-    Route::put('/cards/{id}',     [CardController::class, 'update']);
-    Route::patch('/cards/{id}',   [CardController::class, 'updatePartial']);
-    Route::delete('/cards/{id}',  [CardController::class, 'destroy']);
-});
-
-Route::middleware(['auth:api'])->group(function () {
-    Route::get('/games', [GameController::class, 'index']);
-    Route::post('/games', [GameController::class, 'store']);
-    Route::put('/games/{game}/finish', [GameController::class, 'update']);
-    Route::delete('/games/{game}', [GameController::class, 'destroy']);
-    Route::get('/ranking', [GameController::class, 'ranking']);
-    Route::get('/games/user/{id}', [GameController::class, 'getGamesByUserId']);
-});
 
