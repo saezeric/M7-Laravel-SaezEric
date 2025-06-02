f501216830a08977bc9fa57f915c8ac46338859b
saezeric <128070029+saezeric@users.noreply.github.com>
2025-06-02 13:52:01 +0000

    Limpiamos todo para empezar desde 0

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\StudentController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('/students', [StudentController::class, 'index']);
-Route::post('/students', [StudentController::class, 'store']);
-Route::get('/students/{id}', [StudentController::class, 'show']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
-Route::patch('/students/{id}', [StudentController::class, 'updatePartial']);
-Route::delete('/students/{id}', [StudentController::class, 'destroy']);
